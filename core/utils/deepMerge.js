15fe00f1eb101b8be5c8a1c3324bc007b847b5bc
FernandoYZ <fernandozavala266@gmail.com>
2024-08-03 07:49:13 -0500

    [utils]: added deepMerge function

---
+++ core/utils/deepMerge.js
@@ -0,0 +1,17 @@
+function deepMerge(target, source) {
+    const output = { ...target };
+    if (typeof target === 'object' && typeof source === 'object') {
+      Object.keys(source).forEach(key => {
+        if (typeof source[key] === 'object' && !Array.isArray(source[key])) {
+          if (!target[key]) Object.assign(output, { [key]: source[key] });
+          else output[key] = deepMerge(target[key], source[key]);
+        } else {
+          Object.assign(output, { [key]: source[key] });
+        }
+      });
+    }
+    return output;
+  }
+  
+  export { deepMerge };
+  
