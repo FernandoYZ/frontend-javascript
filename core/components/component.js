c60c7e754281d4078a2277741af23ff9f9e7bc3c
FernandoYZ <fernandozavala266@gmail.com>
2024-08-03 07:51:28 -0500

    [component]: implements the logic to render the component

--- core/components/component.js
+++ core/components/component.js
@@ -2,17 +2,31 @@ class Component {
     constructor(props) {
       this.props = props;
       this.state = {};
+      this.prevState = null;
     }
   
     setState(newState) {
+      this.prevState = { ...this.state };
       this.state = { ...this.state, ...newState };
       this.update();
     }
   
-    update() {}
+    update() {
+      // Implementa la lógica para re-renderizar el componente
+      const renderedOutput = this.render();
+      this._replaceNode(renderedOutput);
+    }
+  
+    _replaceNode(newNode) {
+      const oldNode = this._currentNode;
+      if (oldNode && oldNode.parentNode) {
+        oldNode.parentNode.replaceChild(newNode, oldNode);
+      }
+      this._currentNode = newNode;
+    }
   
     render() {
-      throw "Componentes hijos deben implementar el método render";
+      throw new Error("Componentes hijos deben implementar el método render");
     }
   }
   
