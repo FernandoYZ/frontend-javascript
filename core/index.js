ded125907f7644eff0e3097ec9f03bf5d3e04b20
FernandoYZ <fernandozavala266@gmail.com>
2024-08-03 08:00:12 -0500

    [core]: entry points have been created

---
+++ core/index.js
@@ -0,0 +1,6 @@
+export { default as Component } from './components/component';
+export { render } from './rendering/render';
+export { useState } from './state/useState';
+export { default as EventEmitter } from './events/EventEmitter';
+export { deepClone } from './utils/deepClone';
+export { deepMerge } from './utils/deepMerge';
---
+++ core/utils/index.js
@@ -0,0 +1,2 @@
+export { deepClone } from './deepClone';
+export { deepMerge } from './deepMerge';
