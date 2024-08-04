4925aa9dbc61ec92d665167332267526eb2ebfe0
FernandoYZ <fernandozavala266@gmail.com>
2024-08-03 20:01:35 -0500

    [components]: lifecycle methods have been added to the Component class

--- core/components/component.js
+++ core/components/component.js
@@ -1,34 +1,40 @@
 class Component {
-    constructor(props) {
+  constructor(props) {
       this.props = props;
       this.state = {};
       this.prevState = null;
-    }
-  
-    setState(newState) {
+      this._currentNode = null;
+  }
+
+  setState(newState) {
       this.prevState = { ...this.state };
       this.state = { ...this.state, ...newState };
       this.update();
-    }
-  
-    update() {
-      // Implementa la lógica para re-renderizar el componente
+  }
+
+  update() {
+    // Implementa la lógica para re-renderizar el componente  
+    this.componentDidUpdate(this.prevState, this.state);
       const renderedOutput = this.render();
       this._replaceNode(renderedOutput);
-    }
-  
-    _replaceNode(newNode) {
+  }
+
+  _replaceNode(newNode) {
       const oldNode = this._currentNode;
       if (oldNode && oldNode.parentNode) {
-        oldNode.parentNode.replaceChild(newNode, oldNode);
+          oldNode.parentNode.replaceChild(newNode, oldNode);
       }
       this._currentNode = newNode;
-    }
-  
-    render() {
-      throw new Error("Componentes hijos deben implementar el método render");
-    }
   }
-  
-  export default Component;
-  
+
+  render() {
+      throw new Error("Componentes hijos deben implementar el método render");
+  }
+
+  // Métodos del ciclo de vida
+  componentDidMount() {}
+  componentDidUpdate(prevState, newState) {}
+  componentWillUnmount() {}
+}
+
+export default Component;
