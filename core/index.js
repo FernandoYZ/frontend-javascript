da19a5e010c4e226ba603b31d1a19b8434a1170e
FernandoYZ <fernandozavala266@gmail.com>
2024-08-03 19:35:03 -0500

    [index]: updated entry points for the API.

--- core/index.js
+++ core/index.js
@@ -4,3 +4,6 @@ export { useState } from './state/useState';
 export { default as EventEmitter } from './events/EventEmitter';
 export { deepClone } from './utils/deepClone';
 export { deepMerge } from './utils/deepMerge';
+export { default as apiClient } from './api/apiClient';
+export { validateJsonResponse } from './api/responseValidator';
+export { API_ROUTES } from './api/routes';
