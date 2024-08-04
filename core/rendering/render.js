b45124e22a224499757d0ff3c6a5c1d314579b49
FernandoYZ <fernandozavala266@gmail.com>
2024-08-04 02:27:31 -0500

    [delete]: deleting the rendering folder Actualmente se tiene ya una clase Renderer que se encarga del renderizado, tener una función sería redundante para el proyecto.

--- core/rendering/render.js
+++
@@ -1,9 +0,0 @@
-function render(component, container) {
-    container.innerHTML = '';
-    const element = component.render();
-    container.appendChild(element);
-    component._currentNode = element;
-  }
-  
-  export { render };
-  
