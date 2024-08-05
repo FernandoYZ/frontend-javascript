cff9e35e8f4ea3b0933f9f0a79ee4d694b84cd31
FernandoYZ <fernandozavala266@gmail.com>
2024-08-04 22:18:16 -0500

    [index]: import and export libraries

---
+++ libs/index.js
@@ -0,0 +1,27 @@
+import axios from './axios.min.js';
+import dayjs from './dayjs.min.js';
+import hyperhtml from './hyperhtml.min.js';
+import $ from './jquery.min.js';
+import _ from './lodash.min.js';
+import mobx from './mobx.min.js';
+import Navigo from './navigo.min.js';
+import numeral from './numeral.min.js';
+import page from './page.min.js';
+import preact from './preact.min.js';
+import validator from './validator.min.js';
+import ApexCharts from './apexcharts/apexcharts.min.js';
+
+export {
+    axios,
+    dayjs,
+    hyperhtml,
+    $,
+    _,
+    mobx,
+    Navigo,
+    numeral,
+    page,
+    preact,
+    validator,
+    ApexCharts
+};
