6ccbfd153de9cc4e0fd9ea5054c9aa0b3970a74d
FernandoYZ <fernandozavala266@gmail.com>
2024-08-04 23:14:57 -0500

    [utils]: documentation has been added

--- core/utils/dataUtils.js
+++ core/utils/dataUtils.js
@@ -1,9 +1,20 @@
 import _ from '../../libs/lodash.min.js';
 
+/**
+ * Clona profundamente un objeto.
+ * @param {Object} obj - El objeto a clonar.
+ * @returns {Object} - El objeto clonado.
+ */
 function deepClone(obj) {
     return _.cloneDeep(obj);
 }
 
+/**
+ * Fusiona profundamente dos objetos.
+ * @param {Object} target - El objeto objetivo.
+ * @param {Object} source - El objeto fuente.
+ * @returns {Object} - El objeto fusionado.
+ */
 function deepMerge(target, source) {
     return _.merge({}, target, source);
 }
--- core/utils/numberUtils.js
+++ core/utils/numberUtils.js
@@ -1,13 +1,29 @@
 import numeral from '../../libs/numeral.min.js';
 
+/**
+ * Formatea un valor como moneda.
+ * @param {number} value - El valor a formatear.
+ * @returns {string} - El valor formateado como moneda.
+ */
 function formatCurrency(value) {
     return numeral(value).format('$0,0.00');
 }
 
+/**
+ * Formatea un valor como porcentaje.
+ * @param {number} value - El valor a formatear.
+ * @returns {string} - El valor formateado como porcentaje.
+ */
 function formatPercentage(value) {
     return numeral(value).format('0.00%');
 }
 
+/**
+ * Formatea un valor con un formato específico.
+ * @param {number} value - El valor a formatear.
+ * @param {string} format - El formato a usar.
+ * @returns {string} - El valor formateado.
+ */
 function formatNumber(value, format) {
     return numeral(value).format(format);
 }
