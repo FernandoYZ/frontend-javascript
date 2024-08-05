2187e4e05e86bd48a21e8b3d1ef8d6db373f8757
FernandoYZ <fernandozavala266@gmail.com>
2024-08-04 23:15:41 -0500

    [utils]: centralization of exports of all utilities

--- core/utils/deepClone.js
+++
@@ -1,6 +0,0 @@
-function deepClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-  }
-  
-  export { deepClone };
-  
--- core/utils/index.js
+++ core/utils/index.js
@@ -1,5 +1,3 @@
-export { deepClone } from './deepClone';
-export { deepMerge } from './deepMerge';
 export * from './domUtils';
 export * from './dataUtils';
 export * from './numberUtils';
